90703ef4cdce756b981444f7cc5ac850f5ada902
Sarah French <sarah@sarahfrench.co.uk>
2019-12-01 15:02:16 +0000

    Vue component that uses the functions I set up in first version of Life

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,181 +1,216 @@
 <template>
   <div id="game">
+    <h1>Game of Life</h1>
+    <div class="game__instructions">
+      <div class="game__instructions--left">
+        <p>This is an implementation of <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life">Conway's Game of Life</a></p>
+        <p>Alive cells are black, dead cells are white.</p>
+        <p>If you click on a cell you can toggle it be alive/dead</p>
+        <p>Use the controls to the right to help set up the game</p>
+
+      </div>
+      <div class="game__instructions--right">
+        <p>Enter a number here to resize the board:</p>
+        <input id="dimensions" type="text" name="dimensions" value="" v-model="dimensions">
+        <button type="button" name="button" v-on:click="resizeGrid">Go</button>
+
+        <button class="d-block game__setup-button" type="button" name="button" v-on:click="seedLife">Randomly seed life</button>
+        <button class="d-block game__setup-button" type="button" v-on:click="resizeGrid"> Remove all life from the game</button>
+
+      </div>
+    </div>
+
+    <div v-if="!searchForAnyLife">
+      <p class="danger-text">There are no living cells! Add some following the instructions above</p>
+    </div>
+
+    <div v-if="searchForAnyLife">
+      <button class="d-block game__turn-button highlight" type="button" v-on:click="takeTurn"> Take a turn</button>
+    </div>
+
+
+
     <div class="board">
-      <div v-for="(row, rowIndex) in game.currentState" v-bind:key="row-rowIndex" class="board__row">
-        <div v-for="(element, cellIndex) in row" v-bind:key="cell-rowIndex-cellIndex" class="board__cell">
-            {{element}}
+      <div v-for="(row, rowIndex) in currentState" v-bind:key="rowIndex" class="board__row">
+        <div
+          v-for="(element, cellIndex) in row"
+          v-bind:key="cellIndex"
+          class="board__cell"
+          v-bind:class="{ alive: element.alive}"
+          v-on:click="changeCellStatus(cellIndex, rowIndex)"
+        >
         </div>
       </div>
     </div>
-    <button type="button" name="button" v-on:click="game.takeTurn()" >Take a turn</button>
-    <input v-bind="turns" type="text" name="turns" value="">
   </div>
 </template>
 
 
 <script>
-class GameOfLife {
-
-  constructor(board){
-    if(!this.initialStateValid(board)){
-      throw new Error('Make sure the supplied board is 2D, with consistent row lengths');
-    }
-
-    this.currentState = board;
-    this.largestYCoordinate = board.length - 1;
-    this.largestXCoordinate = board[0].length - 1;
-    this.futureState;
-    this.turns = 0;
-
-    this.resetFutureState();
-
-  }
-
-  initialStateValid(board){
-    if( !board || board.length < 2){
-      //board absent or has less than 2 rows
-      return false;
-    } else {
-      //check for consistent row length
-      let rowsConsistentLength = true;
-
-      board.forEach( row => {
-        rowsConsistentLength = (row.length != board[0].length) ? false : rowsConsistentLength;
-      })
-
-      return rowsConsistentLength;
-    }
-  }
-
-  resetFutureState(){
-    this.futureState = [];
-    for(let i=0; i <= this.largestYCoordinate; i++){
-      this.futureState[i] = [];
-    }
-  }
-
-  takeTurn(){
-
-    this.currentState.forEach( (row, y) => {
-      row.forEach( (cell, x) => {
-          //cycle through every poisition
-          if(this.isCellAlive(x,y)){
-            //position is alive, should it die?
-            let futureValue = this.shouldCellDie(x,y) ? 0 : 1;
-            this.futureState[y].push(futureValue);
-          } else {
-            //position is dead, should it become alive?
-            let futureValue = this.shouldCellBecomeAlive(x,y) ? 1 : 0;
-            this.futureState[y].push(futureValue);
-          }
-      })
-    })
-
-    this.currentState = this.futureState;
-    this.turns += 1;
-    this.resetFutureState()
-  }
-
-  shouldCellDie(x,y){
-
-    let neighbours = this.numberOfLivingNeighbours(x,y);
-
-    /*
-    Return true, die, if fewer than two OR more than 3 neighbours.
-    Otherwise, false; don't die.
-    */
-    return neighbours < 2 || neighbours > 3;
-  }
-
-  shouldCellBecomeAlive(x,y){
-    let neighbours = this.numberOfLivingNeighbours(x,y);
-    /*
-    Return true, live if exactly 3 neighbours.
-    Otherwise, false; don't live.
-    */
-    return neighbours === 3;
-  }
-
-  numberOfLivingNeighbours(x,y){
-    let neighbourVectors = [
-      [1,1],
-      [1,0],
-      [0,1],
-      [-1,1]
-    ];
-
-    let aliveNeighbours = 0;
-
-    neighbourVectors.forEach( vector => {
-      //positive side
-      let neighbourX = x + vector[0];
-      let neighbourY = y + vector[1];
-
-      if(
-        neighbourY >= 0 &&
-        neighbourX >= 0 &&
-        neighbourY <= this.largestYCoordinate &&
-        neighbourX <= this.largestXCoordinate
-      ){
-        //coordinates are in bounds
-        if (this.currentState[neighbourY][neighbourX]){
-          aliveNeighbours += 1;
-        }
-      }
-
-      //negative side
-      neighbourX = x - vector[0];
-      neighbourY = y - vector[1];
-
-      if(
-        neighbourY >= 0 &&
-        neighbourX >= 0 &&
-        neighbourY <= this.largestYCoordinate &&
-        neighbourX <= this.largestXCoordinate
-      ){
-        //coordinates are in bounds
-        if (this.currentState[neighbourY][neighbourX]){
-          aliveNeighbours += 1;
-        }
-      }
-    })
-
-    return aliveNeighbours;
-  }
-
-  searchForAnyLife(){
-    let lifeFound = false;
-    this.currentState.forEach( row => {
-      row.forEach( cell => {
-          lifeFound = cell ? true : lifeFound;
-      })
-    })
-    return lifeFound;
-  }
-
-  isCellAlive(x,y){
-    return this.currentState[y][x] === 1;
-  }
-}
 
 export default {
   name: 'GameOfLife',
   data(){
     return {
-      game: new GameOfLife([[0,1,0],[0,1,0],[0,1,0]]),
-      turns: 0
+      currentState: [],
+      futureState: [],
+      turns: 0,
+      dimensions: 20
     }
   },
-  mounted(){
-
+  computed:{
+    largestYCoordinate: function(){
+      return this.currentState.length - 1;
+    },
+    largestXCoordinate: function(){
+      return this.currentState[0].length - 1;
+    },
+    searchForAnyLife: function(){
+      let lifeFound = false;
+      this.currentState.forEach( row => {
+        row.forEach( cell => {
+            lifeFound = cell.alive ? true : lifeFound;
+        })
+      })
+      return lifeFound;
+    },
+ },
+ created(){
+   this.resizeGrid();
+   console.log("GameOfLife component is loaded");
+ },
+ watch:{
+   dimensions: function(val){
+     if(val < 2){
+       this.dimensions = 2;
+     }
+   }
  },
  methods:{
-   takeNTurns(n){
-     for(let i = 0; i < n; i++){
-       setTimeout( function(){
-         this.game.takeTurn();
-       }, 1000)
+   resizeGrid(){
+     this.currentState = [];
+     for (let i =0; i < this.dimensions; i++){
+       this.currentState.push([]);
+       for (let j =0; j < this.dimensions; j++){
+         this.currentState[i].push({alive:false})
+       }
      }
+     this.resetFutureState();
+   },
+   seedLife(){
+     for (let i =0; i < this.currentState.length; i++){
+       for (let j =0; j < this.currentState[i].length; j++){
+         let life = (Math.random()> 0.5);
+         /*
+            Need to iterate through every single cell and set the property using
+            the $set method, otherwise Vue doesn't re-render the component.
+            This is because Vue cannot deted changes in property values, see
+            here: https://vuejs.org/v2/guide/reactivity.html#How-Changes-Are-Tracked
+         */
+         this.$set(this.currentState[i][j], 'alive', life);
+       }
+     }
+   },
+   changeCellStatus(x,y){
+     let cell = this.currentState[y][x];
+     cell.alive = !cell.alive;
+   },
+   resetFutureState(){
+     this.futureState = [];
+     for(let i=0; i <= this.largestYCoordinate; i++){
+       this.futureState[i] = [];
+     }
+   },
+   takeTurn(){
+
+     this.currentState.forEach( (row, y) => {
+       row.forEach( (cell, x) => {
+           //cycle through every poisition
+           if(cell.alive){
+             //position is alive, should it die?
+             let futureValue = this.shouldCellDie(x,y) ? {alive: false} : {alive: true};
+             this.futureState[y].push(futureValue);
+           } else {
+             //position is dead, should it become alive?
+             let futureValue = this.shouldCellBecomeAlive(x,y) ? {alive: true} : {alive: false};
+             this.futureState[y].push(futureValue);
+           }
+       })
+     })
+
+     this.currentState = this.futureState;
+     this.turns += 1;
+     this.resetFutureState()
+   },
+
+   shouldCellDie(x,y){
+
+     let neighbours = this.numberOfLivingNeighbours(x,y);
+
+     /*
+     Return true, die, if fewer than two OR more than 3 neighbours.
+     Otherwise, false; don't die.
+     */
+     return neighbours < 2 || neighbours > 3;
+   },
+
+   shouldCellBecomeAlive(x,y){
+     let neighbours = this.numberOfLivingNeighbours(x,y);
+     /*
+     Return true, live if exactly 3 neighbours.
+     Otherwise, false; don't live.
+     */
+     return neighbours === 3;
+   },
+
+   numberOfLivingNeighbours(x,y){
+     let neighbourVectors = [
+       [1,1],
+       [1,0],
+       [0,1],
+       [-1,1]
+     ];
+
+     let aliveNeighbours = 0;
+
+     neighbourVectors.forEach( vector => {
+       //positive side
+       let neighbourX = x + vector[0];
+       let neighbourY = y + vector[1];
+
+       if(
+         neighbourY >= 0 &&
+         neighbourX >= 0 &&
+         neighbourY <= this.largestYCoordinate &&
+         neighbourX <= this.largestXCoordinate
+       ){
+         //coordinates are in bounds
+         if (this.currentState[neighbourY][neighbourX].alive){
+           aliveNeighbours += 1;
+         }
+       }
+
+       //negative side
+       neighbourX = x - vector[0];
+       neighbourY = y - vector[1];
+
+       if(
+         neighbourY >= 0 &&
+         neighbourX >= 0 &&
+         neighbourY <= this.largestYCoordinate &&
+         neighbourX <= this.largestXCoordinate
+       ){
+         //coordinates are in bounds
+         if (this.currentState[neighbourY][neighbourX].alive){
+           aliveNeighbours += 1;
+         }
+       }
+     })
+
+     return aliveNeighbours;
+   },
+   isCellAlive(x,y){
+     return this.currentState[y][x].alive;
    }
 
  }
@@ -184,16 +219,65 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.board {
-  display: grid;
+
+.game__instructions {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: baseline;
 }
+
+.game__instructions--left {
+  padding: 0.5em;
+  margin-right: 1em;
+}
+
+.game__setup-button {
+  margin: 1em auto;
+  width: 100%;
+}
+
+.game__turn-button {
+  font-size: 1em;
+  padding: 10px 5px;
+  margin: 1em auto;
+  border: solid 1px #61ECFC;
+  border-radius: 10px;
+}
+
+.game__instructions--right {
+  padding: 0.5em;
+  margin-left: 1em;
+}
+
+.board {
+  margin-top: 3em;
+  margin-bottom: 5em;
+}
+
+.board__row{
+  margin: 0;
+}
+
 .board__cell{
+  margin: 0;
   width: 25px;
   height: 25px;
   display: inline-block;
-  border: solid 1px black;
+  border: solid 1px grey;
+  margin-bottom: -6px;
 }
+
 .alive {
   background-color: black;
 }
+
+.d-block {
+  display: block;
+}
+
+.danger-text {
+  color: #FC6C61;
+}
+
 </style>
